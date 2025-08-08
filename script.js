b0c0fad5d68acdbb7179904fffbb5d6d1449b790
이호준 <hjsj23430@naver.com>
2025-08-08 23:18:19 +0900

    refactor: js에서 렌더링

--- script.js
+++ script.js
@@ -6,5 +6,20 @@ document.addEventListener('DOMContentLoaded', () => {
         "이사짐", "알바", "근처 맛집", "투표", "동네친구"
     ];
 
+    const listContainer = document.querySelector('.popular-search-list');
+    popularKeywords.forEach(keyword => {
+        const li = document.createElement('li');
+        const button = document.createElement('button');
+        button.className = 'popular-search-button';
+        button.textContent = keyword;
 
+        button.addEventListener('click', () => {
+            alert(`인기 검색어 "${keyword}"를 클릭했습니다.`);
+        });
+
+        li.appendChild(button);
+        listContainer.appendChild(li);
+    });
+
+    
 });
